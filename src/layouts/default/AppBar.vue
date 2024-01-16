f2d6f9caafe9bfcfcd31368d858e1a1d863524b4
sj <slaayer60@gmail.com>
2024-01-16 13:34:52 +0900

    feat: mui icon 적용

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,10 +1,35 @@
 <template>
-  <v-app-bar flat>
+  <v-app-bar>
     <v-app-bar-nav-icon/>
     <v-app-bar-title>Simple Messenger</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <!--    popover user info? setting?-->
+    <v-menu location="bottom">
+      <template v-slot:activator="{ props }">
+        <v-icon size="x-large" v-bind="props" :icon="mdiAccountCircle"></v-icon>
+      </template>
+      <v-card min-width="300">
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
+            title="John Leider"
+            subtitle="Founder of Vuetify"
+          >
+          </v-list-item>
+          <v-list-item>
+            <v-btn color="primary">
+              <v-icon start :icon="mdiCog"></v-icon>
+              Settings
+            </v-btn>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-  //
+  // user
+  import {mdiAccountCircle, mdiCog} from '@mdi/js'
+
 </script>
--- src/modules/auth/dto.ts
+++ src/modules/auth/dto.ts
@@ -1,4 +1,3 @@
-
 export class SignUpDto{
   name: string;
   email: string;
