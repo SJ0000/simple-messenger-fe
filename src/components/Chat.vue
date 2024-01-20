f3144fa7f30e0bf0a0088d2abe4ba938e9120560
sj <slaayer60@gmail.com>
2024-01-20 23:34:57 +0900

    feat: message ui

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,24 +1,78 @@
 <template>
   <div class="d-flex flex-column w-100 h-100">
-    <div style="height: 30px">
+    <header style="height: 30px">
       <h2>cat</h2>
-    </div>
-    <div class="h-100">
-
-    </div>
-    <div class="d-flex mb-2" style="height: 50px">
-      <v-icon :icon="mdiMessage"></v-icon>
+    </header>
+    <v-virtual-scroll
+      class="ma-1 bg-amber"
+      :height="100"
+      :items="messages"
+    >
+      <template v-slot:default="{ item }">
+        <v-list-item
+          prepend-avatar="https://cdn.vuetifyjs.com/images/lists/1.jpg"
+          class="ma-2"
+        >
+            <div>
+              <h4>{{ item.senderId }}</h4>
+              <h4>{{ item.content}}</h4>
+              <h4>{{item.sentAt}}</h4>
+            </div>
+        </v-list-item>
+        <v-list-item
+          append-avatar="https://cdn.vuetifyjs.com/images/lists/1.jpg"
+          class="ma-2"
+        >
+            <div class="text-right">
+              <h4>{{ item.senderId }}</h4>
+              <h4>{{ item.content}}</h4>
+              <h4>{{item.sentAt}}</h4>
+            </div>
+        </v-list-item>
+      </template>
+    </v-virtual-scroll>
+    <div class="d-flex">
+      <v-icon :icon="mdiMessage" size="45" class="mr-2 mt-2"></v-icon>
       <v-textarea label="message" rows="1" row-height="15"></v-textarea>
+      <v-btn class="ml-2 mt-3" @click="addItemTest">
+        Send
+      </v-btn>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import {mdiMessage} from "@mdi/js"
+
+// 사용자 정보를 사전에 가지고 있어야 함
+const myId = 1;
+
+const messages = [
+  {
+    id: 10,
+    senderId: 1,
+    content: "Hello, everyone!",
+    sentAt: "10:30 AM",
+  },
+  {
+    id: 11,
+    senderId: 2,
+    content: "Hi Dog",
+    sentAt: "10:30 AM",
+  },
+]
+
+function addItemTest(){
+  messages.push(
+    {
+      id: 11,
+      senderId: 2,
+      content: "messageN",
+      sentAt: "10:31 AM",
+    }
+  )
+  console.log(messages)
+}
 </script>
-
-
-
-
 <style scoped>
 </style>
