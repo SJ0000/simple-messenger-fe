5fbfb808878d407504d12c02e1edab70382847f5
sj <slaayer60@gmail.com>
2024-02-13 17:12:14 +0900

    refactor: chat ui 개선

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -7,51 +7,31 @@
       <header class="d-flex justify-space-between">
         <span class="text-h4">{{ chatRoom.name }}</span>
         <v-btn @click="onInvitationLinkClick">invitation link</v-btn>
-        <InvitationLinkDialog ref="dialog"/>
+        <InvitationLinkDialog ref="dialog" />
       </header>
-      <v-virtual-scroll
-        id="virtual-scroll"
-        class="ma-1"
-        :items="messages"
-        height="600"
-        item-height="50"
-      >
+      <v-virtual-scroll id="virtual-scroll" class="ma-1" :items="messages" height="600" item-height="50">
         <template v-slot:default="{ item }">
-          <v-list-item
-            :id="item.id"
-            v-if="item.senderId === user.id"
-            append-avatar="https://cdn.vuetifyjs.com/images/lists/1.jpg"
-            class="ma-2"
-          >
+          <v-list-item v-if="item.senderId === user.id" :id="item.id" :append-avatar="findUser(item.senderId).avatarUrl"
+            class="ma-2">
             <div class="text-right">
-              <h4>{{ findUserName(item.senderId) }}</h4>
-              <h4>{{ item.content }}</h4>
-              <h4>{{ item.sentAt }}</h4>
+              <div class="text-subtitle-2 font-weight-bold">{{ findUser(item.senderId).name }}</div>
+              <div class="text-body-2">{{ item.content }}</div>
+              <div class="text-caption font-italic">{{ getFormattedDate(item.receivedAt) }}</div>
             </div>
           </v-list-item>
-          <v-list-item
-            v-else
-            prepend-avatar="https://cdn.vuetifyjs.com/images/lists/1.jpg"
-            class="ma-2"
-          >
+          <v-list-item v-else :id="item.id" :prepend-avatar="findUser(item.senderId).avatarUrl" class="ma-2">
             <div>
-              <h4>{{ findUserName(item.senderId) }}</h4>
-              <h4>{{ item.content }}</h4>
-              <h4>{{ item.sentAt }}</h4>
+              <div class="text-subtitle-2 font-weight-bold">{{ findUser(item.senderId).name }}</div>
+              <div class="text-body-2">{{ item.content }}</div>
+              <div class="text-caption font-italic">{{ getFormattedDate(item.receivedAt) }}</div>
             </div>
           </v-list-item>
         </template>
       </v-virtual-scroll>
       <div class="d-flex">
         <v-icon :icon="mdiMessage" size="45" class="mr-2 mt-2"></v-icon>
-        <v-textarea
-          variant="outlined"
-          rows="1"
-          row-height="15"
-          label="message"
-          v-model="content"
-          @keydown.enter.exact.prevent="pressEnterHandler"
-        />
+        <v-textarea variant="outlined" rows="1" row-height="15" label="message" v-model="content"
+          @keydown.enter.exact.prevent="pressEnterHandler" />
         <v-btn class="ml-2 mt-3" @click="onSendButtonClick">
           Send
         </v-btn>
@@ -60,16 +40,17 @@
   </div>
 </template>
 <script setup lang="ts">
-import {mdiMessage} from "@mdi/js"
-import {ChatRoom, SentMessage} from "@/modules/chat/interface";
-import {reactive, Ref, ref, watch} from "vue";
-import {MessageClient} from "@/modules/chat/message-client";
-import {authenticationStore} from "@/store/authentication";
-import {chatRoomStore} from "@/store/chatroom";
-import {ApiClient} from "@/modules/common/api-client";
+import { mdiMessage } from "@mdi/js"
+import { ChatRoom, SentMessage } from "@/modules/chat/interface";
+import { reactive, Ref, ref, watch } from "vue";
+import { MessageClient } from "@/modules/chat/message-client";
+import { authenticationStore } from "@/store/authentication";
+import { chatRoomStore } from "@/store/chatroom";
+import { ApiClient } from "@/modules/common/api-client";
 import InvitationLinkDialog from "@/components/dialog/InvitationLinkDialog.vue";
-import {VVirtualScroll} from "vuetify/components";
-import {useGoTo} from "vuetify";
+import { VVirtualScroll } from "vuetify/components";
+import { useGoTo } from "vuetify";
+import { User } from "@/modules/user/interface";
 
 
 const chatRoom: Ref<ChatRoom> = ref(chatRoomStore().selected)
@@ -88,8 +69,8 @@ function createMessage(): SentMessage {
   }
 }
 
-function findUserName(userId: number): string {
-  return chatRoomStore().selected.users.find(user => user.id === userId)?.name ?? "unknown"
+function findUser(userId: number): User {
+  return chatRoomStore().selected.users.find(user => user.id === userId) ?? { id: -1, name: "Unknown", avatarUrl: "" }
 }
 
 function sendMessageAndTextResetIfContentNotEmpty() {
@@ -103,7 +84,7 @@ function sendMessageAndTextResetIfContentNotEmpty() {
 async function onInvitationLinkClick() {
   const chatRoomId = chatRoom.value.id
   const invitation = await ApiClient.getInstance().createInvitation(chatRoomId)
-  dialog.value?.open(`http://localhost:3000/invite/${invitation.id}`)
+  dialog.value?.open(`${import.meta.env.VITE_API_URL}/invite/${invitation.id}`)
 }
 
 function onSendButtonClick() {
@@ -127,9 +108,25 @@ const scrollOptions = {
 
 
 watch(messages, (current, prev) => {
-  goTo(10000,scrollOptions)
+  goTo(10000, scrollOptions)
 })
 
+
+function getFormattedDate(date: Date): string {
+  const d = new Date(date)
+  const year = d.getFullYear()
+  const month = add0IfLessThan10(d.getMonth())
+  const day = add0IfLessThan10(d.getDay())
+  const hour = add0IfLessThan10(d.getHours())
+  const minutes = add0IfLessThan10(d.getMinutes())
+  return `${year}-${month}-${day} ${hour}:${minutes}`
+}
+
+function add0IfLessThan10(num: number): string {
+  if (num <= 9)
+    return `0${num}`
+  return `${num}`
+}
+
 </script>
-<style scoped>
-</style>
+<style scoped></style>
