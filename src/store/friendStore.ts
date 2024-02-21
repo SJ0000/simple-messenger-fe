ad61727e31cbd172b988c2e9d228a4f1eba53ad3
sj <slaayer60@gmail.com>
2024-02-22 00:16:22 +0900

    refactor: friend store setup 방식으로 변경

--- src/store/friendStore.ts
+++ src/store/friendStore.ts
@@ -1,35 +1,30 @@
 import { defineStore } from "pinia";
 import { User } from "@/modules/user/interface";
+import { Ref, ref } from "vue";
 
-interface friendState {
-  friends: Map<number, User>;
-}
+export const friendStore = defineStore(
+  "friend",
+  () => {
+    const friends: Ref<Map<number, User>> = ref(new Map<number, User>());
 
-export const friendStore = defineStore("friend", {
-  state: (): friendState => {
-    return {
-      friends: new Map<number, User>(),
-    };
-  },
-
-  actions: {
-    initialize(users: Array<User>) {
+    function initialize(users: Array<User>) {
       users.forEach((user) => {
-        this.friends.set(user.id, user);
+        friends.value.set(user.id, user);
       });
-    },
+    }
 
-    find(userId: number): User {
-      const friend = this.friends.get(userId);
+    function find(userId: number): User {
+      const friend = friends.value.get(userId);
       if (friend === undefined)
         throw Error(`Friend(userId = ${userId}) not found`);
 
       return friend;
-    },
+    }
 
-    getFriends(): Array<User> {
-      return Array.from(this.friends.values());
-    },
+    function getFriends(): Array<User> {
+      return Array.from(friends.value.values());
+    }
+    return { friends, initialize, find, getFriends };
   },
-  persist: false,
-});
+  { persist: false }
+);
